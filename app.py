589d84e2e141ac656489f2f60be4d53d0f2918a2
Derry <mingrui.li3@gmail.com>
2016-05-12 12:55:04 -0400

    skeleton for paths

---
+++ app.py
@@ -0,0 +1,18 @@
+import utils
+import json
+from threading import Timer
+from flask import Flask, render_template, request, redirect, url_for, session
+app = Flask(__name__)
+
+@app.route("/")
+
+@app.route("/register")
+
+@app.route("/login")
+
+@app.route("/calender")
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0", port=8000)
