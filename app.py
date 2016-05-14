6788d3667737ed0932bb798ee4376657ee309a4c
Derry <mingrui.li3@gmail.com>
2016-05-14 17:34:21 -0400

    loading home, need to figure out chrome problems

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
-import utils
-import json
-from threading import Timer
+
 from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 
 @app.route("/")
+def start():
+    return render_template("home.html")
 
 @app.route("/register")
+def register():
+    return null
 
 @app.route("/login")
+def login():
+    return null
 
 @app.route("/calender")
-
+def calender():
+    return null
 
 if __name__ == "__main__":
-    app.debug = True
+    app.debug = True;
     app.run(host="0.0.0.0", port=8000)
