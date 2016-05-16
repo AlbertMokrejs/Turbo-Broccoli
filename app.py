190af7b345d9b6b2bc18f7d1964bd25a781950a8
Derry <mingrui.li3@gmail.com>
2016-05-16 15:02:37 -0400

    added more routes, still need html files and functions

--- app.py
+++ app.py
@@ -2,21 +2,35 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 
+#home route, subject to change what it loads
 @app.route("/")
 def start():
     return render_template("home.html")
 
+#route to register a user
 @app.route("/register")
 def register():
     return null
 
+#route to login, subject to change
 @app.route("/login")
 def login():
     return null
 
+#route to calender, and potentinally the reservation form
 @app.route("/calender")
 def calender():
     return null
+
+#overall route to pass something to backend from front without reloading page
+@app.route("/get_functions")
+def get_functions():
+    return null;
+
+#overall route to change something backend without reloading page
+@app.route("/set_functions")
+def set_functions():
+    return null;
 
 if __name__ == "__main__":
     app.debug = True;
