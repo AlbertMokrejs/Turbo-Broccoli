48b60f50c15ba9d45bc929aeb874b13140927de7
Albert Mokrejs <albertmokrejs@gmail.com>
2016-05-19 12:56:39 -0400

    Update TMP.py

--- TMP.py
+++ TMP.py
@@ -36,9 +36,9 @@ def runSQL( doesReturn, q):
       conn.commit()
 
 def register(email, name, club, password):
-   isTaken = len(findMatching("Users",{"email":email})) > 0
+   isTaken = len(findMatching("Users",{"email":'"' + email + '"'})) > 0
    if not isTaken:
-      insertValue("Users",[name,email,password,base64.b64encode(marshal.dumps([])),0,club])
+      insertValue("Users",[name,'"' + email + '"',password,base64.b64encode(marshal.dumps([])),0,club])
       return True
    return False
    
