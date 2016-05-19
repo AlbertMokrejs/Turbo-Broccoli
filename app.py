82b0365eeca3b95d10072d06807e42f84e0b8d7e
Derry <mingrui.li3@gmail.com>
2016-05-19 11:56:02 -0400

    login and register should technically work, problem with backend function, rewrote links on the homepage, added small stuff to login and register html pages

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
 from flask import Flask, render_template, request, redirect, url_for, session
+import TMP
 app = Flask(__name__)
 
 #home route, subject to change what it loads
@@ -8,14 +9,63 @@ def start():
     return render_template("home.html")
 
 #route to register a user
-@app.route("/register")
+@app.route("/register", methods = ["GET","POST"])
 def register():
-    return render_template("register.html")
+    print("reg.1")
+    print(request.method)
+    if request.method == "POST":
+        print("reg.2")
+        if str(request.form["button"]) == "Register":
+            print("reg.2.1")
+            email = str(request.form["uemail"])
+            name = str(request.form["uname"])
+            cname = str(request.form["clubname"])
+            password1 = str(request.form["password1"])
+            passwordcheck = str(request.form["passwordcheck"])
+            if password1 == passwordcheck:
+                print("reg.2.1.1")
+                print(email +","+name+","+cname+","+password1+","+passwordcheck)
+                if (TMP.register(email,name,cname,password1)):
+                    print("reg.2.1.1.1")
+                    session["username"] = email
+                    return redirect(url_for('/', email = email))
+                else:
+                    print("reg.2.1.1.2")
+                    return render_template("register.html", text = "The email is already taken")
+            else:
+                print("reg.2.1.2")
+                return render_template("register.html", text = "Passwords do not match")
+        else:
+            print("reg.2.2")
+            return redirect(url_for('/'))
+    else:
+        print("reg.3")
+        return render_template("register.html")
 
+    
 #route to login, subject to change
-@app.route("/login")
+@app.route("/login", methods = ["GET","POST"])
 def login():
-    return render_template("login.html")
+    print("log.1")
+    if request.method == "POST":
+        print("log.2")
+        if str(request.form["button"]) == "Login":
+            print("log.2.1")
+            email = str(request.form["umail"])
+            password = str(request.form["password"])
+            print(email)
+            print(password)
+            if(TMP.login(email,password)):
+                print("log.2.1.1")
+                session["username"] = email
+                return redirect(url_for('/', email=email))
+            else:
+                print("log.2.1.2")
+                return render_template("login.html", text = "Email/Password do not match")
+    else:
+        print("log.3")
+        return render_template("login.html")
+    
 
 
 #overall route to pass something to backend from front without reloading page
@@ -29,5 +79,6 @@ def set_functions():
     return null;
 
 if __name__ == "__main__":
+    app.secret_key= 'asidh19201o231l2k3j'
     app.debug = True;
     app.run(host="0.0.0.0", port=8000)
