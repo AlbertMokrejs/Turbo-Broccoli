2a2dffccb4df96b95759a7b31b108635cb883ca3
TyranitarShawn <shawnli050298@gmail.com>
2016-05-25 13:21:03 -0400

    :]

--- static/form.js
+++ static/form.js
@@ -1,1 +1,14 @@
-
+$(function() {
+    $('.button').click(function() {
+        var ClubName = $('#clubName').val();
+        var Gmail = $('#email').val();
+	var Name = $('#name').val();
+	var Room = $('#room').val();
+	var Date = $('#date').val();
+        $.ajax({
+            url: '/set_functions',
+	    data: {club: ClubName, email: Gmail, name: Name, room: Room, date: Data},
+            }
+        });
+    });
+});
