16a9a12d1421df6d4238c263168db51c5b6c735d
Albert Mokrejs <albertmokrejs@gmail.com>
2016-05-25 12:58:30 -0400

    Update TMP.py

--- TMP.py
+++ TMP.py
@@ -5,25 +5,22 @@ import json
 import time 
 
 def checkGenerate(version):
-   if not os.path.isfile("Calendar.db"): #is it a file
-      connect = sqlite3.connect("Calendar.db") #make it a file
-      curs = connect.cursor() #connect to it
+   if not os.path.isfile("Calendar.db"):
+      connect = sqlite3.connect("Calendar.db")
+      curs = connect.cursor()
       TableList = [["Reservations","club TEXT","email TEXT","name TEXT","room REAL","date TEXT","timeS TEXT","timeE TEXT", "UID REAL"],["Users","user TEXT","email TEXT","password TEXT","reservations BLOB","UID REAL","Club TEXT"],["version","v TEXT"]]
-      for q in TableList: #iterate list
-         makeTable(q[0],q[1:]) #make a table
-      insertValue("version",[version]) #put a version tag
-      print "VERSION UP TO DATE." #print
-   if os.path.isfile("Calendar.db"): #if its a file
-      try: #make an attempt
-         result = runSQL(True, """SELECT * FROM version;""") #get EVERYTHING
-         for r in result: #iterate EVERYTHING
-            if r[0] != version: #if version tag bad
-               print "INVALID VERSION. \n WIPING AND UPDATING." #fuck it, do it again
-               x = False #because variables
-               os.rename("Calendar.db","Archive.db") #archive the file
-               checkGenerate(version) #do it again, for real this time
-      except: #if attempt fucked up
-         pass #don't give a shit
+      for q in TableList: 
+         makeTable(q[0],q[1:]) 
+      insertValue("version",[version])
+      print "VERSION UP TO DATE." 
+   try: 
+      result = runSQL(True, """SELECT * FROM version;""") 
+      for r in result: 
+         if r[0] != version:
+            os.rename("Calendar.db","Archive.db") 
+            checkGenerate(version) 
+   except: 
+      pass 
          
 def runSQL( doesReturn, q):
    conn = sqlite3.connect("Calendar.db")
@@ -71,9 +68,7 @@ def getReservations():
    counter = 0
    while counter < len(final) - 1:
       if checkDates(final[counter][4], final[counter + 1][4]):
-         tmp = final[counter]
-         final[counter] = final[counter + 1]
-         final[counter + 1] = tmp
+         final[counter],final[counter + 1] = final[counter + 1],final[counter]
          counter = 0
       else:
          counter += 1
@@ -82,19 +77,10 @@ def getReservations():
 def checkDates(a,b):
    a = a.split("/")
    b = b.split("/")
-   if a[0] > b[0]:
-      return True
-   if a[0] == b[0]:
-      if a[1] > b[1]:
-         return True
-      if a[1] == b[1]:
-         if a[2] > b[2]:
-            return True
-   return False
+   return a[0] > b[0] or (a[0] == b[0] and a[1] > b[1]) or (a[0] == b[0] and a[1] == b[1] and a[2] > b[2])
 
 def makeTable(name, arg):
    runSQL(False, """CREATE TABLE %s(%s)""" % (name, "".join(str([x for x in arg])[1::][::-1][1::][::-1].split("'"))))
-   
    
 def makeTableJSON(a):
    result = json.loads(a)
@@ -108,17 +94,14 @@ def insertValueJSON(a):
    result = json.loads(a)
    insertValue(result["name"],result["data"])
 
-
 def findMatching(name, arg):
    q = """SELECT * FROM %s""" % (name)
    if len(arg.keys()) > 0:
       q += """ WHERE """
       for x in arg.keys():
          q += "%s.%s = '%s' AND " % (name, x, arg[x])
-      q = q[::-1][4::][::-1] + ";"
-      print q
-   else:
-      q += ";"
+      q = q[::-1][4::][::-1]
+   q += ";"
    result = runSQL(True, q)
    return [r for r in result]
    
@@ -126,19 +109,7 @@ def findMatchingJSON(a):
    result = json.loads(a)
    return findMatching(result["name"],result["data"])
     
-def test():
-   checkGenerate("version0")
-   insertValue("testTable",["a","b"])
-   insertValue("testTableB",[1,"2","3","b"])
-   insertValue("testTableB",[2,"3","4","b"])
-   print findMatching("testTable",{})
-   print findMatching("testTable",{"field1":"'a'"})
-   print findMatching("testTableB",{"a":"1"})
-   print findMatching("testTableB",{"d":"'b'"})
-   
-
-
-#http://stackoverflow.com/questions/9847213/which-day-of-week-given-a-date-python
+ #http://stackoverflow.com/questions/9847213/which-day-of-week-given-a-date-python
 def weekDay(a):
     year, month, day = int(a[0]),int(a[1]),int(a[2])
     offset = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
