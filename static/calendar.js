c9c31288924d31734743750fd87da552107a05c2
sarahjoseph723 <sjoseph@stuy.edu>
2016-05-26 12:45:12 -0400

    month update

--- static/calendar.js
+++ static/calendar.js
@@ -1,6 +1,7 @@
 var d = new Date();
 var todayNum = d.getDate();
 var monthNum = d.getMonth();
+var thisYear = d.getYear();
 
 function findMonth(num) {
     console.log(num);
@@ -43,3 +44,68 @@ function findMonth(num) {
 };
 
 document.getElementById("mesa").caption.innerHTML = findMonth(monthNum);
+
+/*
+//find weekday of the first
+function weekDay(year, month, day) {
+    var offset = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+    var week   = [0,1,2,3,4,5,6];
+    var afterFeb = 1;
+    if (month > 2) {
+	    afterFeb = 0;
+    }
+    var aux = year - 1700 - afterFeb;
+    dayOfWeek  = 5;
+    dayOfWeek += (aux + afterFeb) * 365;
+    dayOfWeek += aux / 4 - aux / 100 + (aux + 100) / 400;
+    dayOfWeek += offset[month - 1] + (day - 1);
+    dayOfWeek %= 7;
+    return week[dayOfWeek];
+};
+
+function fillCal(month) {
+    //only works for 2016 right now
+    var cal = [];
+    for (var x = 0; x < 5; x++) {
+	cal[x] = [];
+	for (var y = 0; y < 7; y++) {
+	    cal[x][y] = 0;
+	}
+    }
+    //cal is a 5 row 7 column 2d array
+    //months with 31 days
+    if (monthNum == 0 || monthNum == 2 || monthNum == 4 || monthNum == 6 || monthNum == 7 || monthNum == 9 || monthNum == 11) { 
+	i = 0; //day ctr
+	while (i < 31) {
+	    dayOfWeek = weekDay(thisYear,monthNum,i);	 
+	    j = 0; //week ctr
+	    while (dayOfweek < 7) {
+		
+	    }
+	    i++;   
+	}
+    }
+    //leap year
+    else if (monthNum == 1 && thisYear%4 == 0) {
+	i = 0;
+	while (i < 29) {
+	    i++;
+	}
+    }
+    //regular february
+    else if (monthNum == 1) {
+	i = 0;
+	while (i < 28) {
+	    i++;
+	}
+    }
+    //30 days
+    else {
+	i = 0;
+	while (i < 30) {
+	    i++;
+	}
+    }
+    return 0;
+};
+*/
