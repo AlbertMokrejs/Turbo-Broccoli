030402ce7fb7d471963a90fc3dea96b59e3caffe
Derry <mingrui.li3@gmail.com>
2016-06-05 11:09:05 -0400

    adding authen functions

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
- 
 from flask import Flask, render_template, request, redirect, url_for, session
 import TMP
 app = Flask(__name__)
 TMP.checkGenerate("1")
-
 app.secret_key= 'asidh19201o231l2k3j'
+
 
 #home route, subject to change what it loads
 @app.route("/", methods = ["GET", "POST"])
@@ -18,6 +17,8 @@ def start():
         print("the court is not in session")
         return render_template("home.html")
 
+
+    
 #route to register a user
 @app.route("/register", methods = ["GET","POST"])
 def register():
@@ -30,10 +31,8 @@ def register():
             passwordcheck = str(request.form["passwordcheck"])
             if password1 == passwordcheck:
                 if (TMP.register(email,name,cname,password1)):
-                    session["username"] = email
-                    #return render_template("register.html", text = "yay")
-                
-                    return redirect(url_for('start'))
+                    TMP.send_email(email, "TBD", "TBD") #send the auth email here
+                    return redirect("/authenticate/" + email + "")
                 else:
                     return render_template("register.html", text = "The email is already taken")
             else:
@@ -44,6 +43,21 @@ def register():
         return render_template("register.html")
 
     
+
+    
+#route to authenticate a user
+@app.route("/authenticate")
+@app.route("/authenticate/<username>")
+def auth(username):
+    if request.method == "POST":
+        if str(request.form["code"]) == TMP.get_auth_code_function:
+            TMP.change_auth_status_function
+            session["username"] = username
+            return redirect(url_for('start'))
+    else:
+        return render_template("authentication.html" username = username)
+    
+            
 #route to login, subject to change
 @app.route("/login", methods = ["GET","POST"])
 def login():
@@ -57,8 +71,13 @@ def login():
             else:
                 return render_template("login.html", text = "Email/Password do not match")
     else:
-        return render_template("login.html")
+        if TMP.check_auth_status_function:
+            return render_template("login.html")
+        else:
+            return redirect("/authenticate/" + email + "")
 
+
+        
 #route to show person reservations
 @app.route("/reservations", methods = ["GET","POST"])
 def reservations():
@@ -68,6 +87,9 @@ def reservations():
             return render_template("reservations.html", reservations = reservations)
     else:
         return redirect(url_for('start'))
+
+
+    
 @app.route("/reservations/<res_id>", methods = ["GET", "POST"])
 def delete_res(res_id):
     if session != {}:
@@ -85,12 +107,15 @@ def logout():
     session.clear()
     return redirect(url_for('start'))
 
-    
+
+
 #overall route to pass something to backend from front without reloading page
 @app.route("/get_functions", methods = ["GET","POST"])
 def get_res():
     print(TMP.getReservations())
     return TMP.getReservations()
+
+
 
 #overall route to change something backend without reloading page
 @app.route("/set_functions", methods = ["GET"])
@@ -107,6 +132,8 @@ def set_res():
         return "false"
 
 
+
+    
 if __name__ == "__main__":
     app.debug = True
     app.run(host="0.0.0.0", port=8000)
